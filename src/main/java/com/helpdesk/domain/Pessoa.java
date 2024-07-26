6cc817cd84cb618d37bde4695ce3aa9f72a30784
igorpardinho <pardinhoigor@gmail.com>
2024-07-26 19:47:21 -0300

    criação da classe abstrata Pessoa

---
+++ src/main/java/com/helpdesk/domain/Pessoa.java
@@ -0,0 +1,79 @@
+package com.helpdesk.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@Entity
+public abstract class Pessoa implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    protected String nome;
+
+    @Column(unique = true)
+    protected String cpf;
+
+    @Column(unique = true)
+    protected String email;
+
+    protected String senha;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "PERFIS")
+    protected Set<Integer> perfis = new HashSet<>();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public Pessoa() {
+        super();
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public Pessoa(Long id, String nome, String cpf, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(Perfil::toEnum).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        this.perfis.add(perfil.getCodigo());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
