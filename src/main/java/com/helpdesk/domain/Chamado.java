5c64c5ddf93fe13782d82b3d4f1dd55aabf735c9
igorpardinho <pardinhoigor@gmail.com>
2024-07-26 19:47:41 -0300

    criação da classe Chamado

---
+++ src/main/java/com/helpdesk/domain/Chamado.java
@@ -0,0 +1,71 @@
+package com.helpdesk.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.helpdesk.domain.enums.Prioridade;
+import com.helpdesk.domain.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+public class Chamado implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataAbertura = LocalDate.now();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataFechamento;
+
+    private Prioridade prioridade;
+    private Status status;
+    private String titulo;
+    private String observacoes;
+
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
+    private Tecnico tecnico;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    public Chamado() {
+        super();
+    }
+
+    public Chamado(Long id, Prioridade prioridade, Status status,
+                   String titulo, String observacoes, Tecnico tecnico, Cliente cliente) {
+        this.id = id;
+        this.prioridade = prioridade;
+        this.status = status;
+        this.titulo = titulo;
+        this.observacoes = observacoes;
+        this.tecnico = tecnico;
+        this.cliente = cliente;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Chamado chamado = (Chamado) o;
+        return Objects.equals(id, chamado.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
